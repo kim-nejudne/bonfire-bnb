20754c0f18fdb5214cb6d6b427944c91ee0fce0d
kim-nejudne <work.august.nejudne@gmail.com>
2025-03-17 18:43:17 +0800

    feature add clerk authentication

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,8 +1,8 @@
+import { LucideUser2 } from "lucide-react";
+
 const UserIcon = () => {
-	return (
-		<div>
-			UserIcon
-		</div>
-	)
+  return (
+    <LucideUser2 className="w-6 h-6 bg-primary rounded-full text-white" />
+  );
 };
 export default UserIcon;
