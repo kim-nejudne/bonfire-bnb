05cd3e473f9adaeae1094687d91dac53b30dfb1e
kim-nejudne <work.august.nejudne@gmail.com>
2025-04-15 22:27:52 +0800

    remove debug stuff from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { toast } from "sonner";
-
 const HomePage = () => {
-  const handleClick = () => {
-    toast("Event has been created",
-      { description: "You have been signed out." }
-    );
-  };
   return (
     <div>
       <h1 className='text-3xl'>HomeAway Project - Starter</h1>
-      <Button variant="outline" onClick={handleClick}>Hello</Button>
     </div>
   );
 };
