17116a2f2e485795ed62e4d81d60b9d9c9f56ebc
kim-nejudne <work.august.nejudne@gmail.com>
2025-04-18 15:03:53 +0800

    update profile action

--- utils/actions.ts
+++ utils/actions.ts
@@ -2,18 +2,31 @@
 
 import { profileSchema } from "./schemas";
 import db from "./db";
-import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
+import { auth, clerkClient, currentUser, User } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { Profile } from "@prisma/client";
+
+const getAuthUser = async (): Promise<User> => {
+  const user = await currentUser();
+
+  if (!user) {
+    throw new Error("User not authenticated");
+  }
+
+  if (!user.privateMetadata?.hasProfile) {
+    redirect("/profile/create");
+  }
+
+  return user;
+};
 
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
 ): Promise<{ message: string }> => {
   try {
-    const user = await currentUser();
-    if (!user) throw new Error("Please login to create a profile");
-
+    const user = await getAuthUser();
     const rawData = Object.fromEntries(formData.entries());
     const validatedFields = profileSchema.parse(rawData);
 
@@ -40,11 +53,48 @@ export const createProfileAction = async (
   redirect("/");
 };
 
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  try {
+    const user = await getAuthUser();
+    const rawData = Object.fromEntries(formData.entries());
+    const validatedFields = profileSchema.parse(rawData);
+    const profile = await db.profile.findUnique({
+      where: {
+        clerkId: user.id,
+      },
+    });
+    if (!profile) {
+      throw new Error("Profile not found");
+    }
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: {
+        ...validatedFields,
+        profileImage: user.imageUrl ?? "",
+      },
+    });
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
+
+    return { message: "Profile updated successfully" };
+  } catch (error) {
+    return {
+      message: error instanceof Error ? error.message : "An error occurred",
+    };
+  }
+};
+
 export const fetchProfileImage = async (): Promise<string | undefined> => {
   try {
-    const user = await currentUser();
-    if (!user) throw new Error("Please login to create a profile");
-
+    const user = await getAuthUser();
     const profile = await db.profile.findUnique({
       where: {
         clerkId: user.id,
@@ -57,6 +107,26 @@ export const fetchProfileImage = async (): Promise<string | undefined> => {
     return profile?.profileImage;
   } catch (error) {
     console.error("Error fetching profile image:", error);
-    return undefined;
+    throw error;
+  }
+};
+
+export const fetchProfile = async (): Promise<Profile> => {
+  try {
+    const user = await getAuthUser();
+    const profile = await db.profile.findUnique({
+      where: {
+        clerkId: user.id,
+      },
+    });
+
+    if (!profile) {
+      throw new Error("Profile not found");
+    }
+
+    return profile;
+  } catch (error) {
+    console.error("Error fetching profile:", error);
+    throw error;
   }
 };
