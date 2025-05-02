77becf1579c9181cc44d7b99ae710cf38700a679
kim-nejudne <work.august.nejudne@gmail.com>
2025-05-02 19:00:23 +0800

    update NavSearch

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,8 +1,47 @@
+"use client";
+
 import { Input } from "../ui/input";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { debounce } from "lodash";
+import { useState, useEffect } from "react";
 
 const NavSearch = () => {
-	return (
-		<Input type="text" placeholder="find a property..." className="max-w-xs dark:bg-muted" />
-	)
+  const searchParams = useSearchParams();
+
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const [search, setSearch] = useState(
+    searchParams.get("search")?.toString() || ""
+  );
+
+  const handleSearch = debounce((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
+  useEffect(() => {
+    if (!searchParams.get("search")) {
+      setSearch("");
+    }
+  }, [searchParams]);
+
+  return (
+    <Input
+      type="search"
+      placeholder="find a property..."
+      className="max-w-xs dark:bg-muted "
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
+      value={search}
+    />
+  );
 };
+
 export default NavSearch;
