71974aeb21ef0e5cee8789794e5cd3b0ecf551da
kim-nejudne <work.august.nejudne@gmail.com>
2025-05-02 19:26:58 +0800

    feat: implement FavoriteToggleButton with sign-in prompt

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -1,7 +1,10 @@
 "use client";
+
 import { ReloadIcon } from "@radix-ui/react-icons";
 import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
 
 type btnSize = "default" | "lg" | "sm";
 
@@ -33,5 +36,21 @@ export const SubmitButton = ({
         text
       )}
     </Button>
+  );
+};
+
+export const CardSignInButton = () => {
+  return (
+    <SignInButton mode="modal">
+      <Button
+        type="button"
+        size="icon"
+        variant="outline"
+        className="p-2 cursor-pointer"
+        asChild
+      >
+        <FaRegHeart />
+      </Button>
+    </SignInButton>
   );
 };
