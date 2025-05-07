91ee53a067bcb1567d8930cc8a34fcb8df8f18ab
kim-nejudne <work.august.nejudne@gmail.com>
2025-05-07 23:31:46 +0800

    bookingn calendar and container components

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+"use client";
+import { Calendar } from "@/components/ui/calendar";
+import { useEffect, useState } from "react";
+import { DateRange } from "react-day-picker";
+import { useProperty } from "@/utils/store";
+
+import { defaultSelected } from "@/utils/calendar";
 
 const BookingCalendar = () => {
-  return <div>BookingCalendar</div>;
+  const currentDate = new Date();
+
+  const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
+
+  useEffect(() => {
+    useProperty.setState({ range });
+  }, [range]);
+
+  return (
+    <Calendar
+      mode="range"
+      defaultMonth={currentDate}
+      selected={range}
+      onSelect={setRange}
+      className="mb-4"
+    />
+  );
 };
 
 export default BookingCalendar;
