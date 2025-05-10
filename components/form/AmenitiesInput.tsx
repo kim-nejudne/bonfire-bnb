39a822a2b6f3ffcf8b6ed74c6bd2792705ee6eae
kim-nejudne <work.august.nejudne@gmail.com>
2025-05-10 14:45:40 +0800

    update how AmenitiesInput handles defaultValue

--- components/form/AmenitiesInput.tsx
+++ components/form/AmenitiesInput.tsx
@@ -1,14 +1,22 @@
 "use client";
 
 import { useState } from "react";
-import { conservativeAmenities, Amenity } from "@/utils/amenities";
+import { conservativeAmenities as amenities, Amenity } from "@/utils/amenities";
 import { Checkbox } from "@/components/ui/checkbox";
 
-const AmenitiesInput = ({ defaultValue }: { defaultValue?: Amenity[] }) => {
+const AmenitiesInput = ({
+  defaultValue = amenities,
+}: {
+  defaultValue?: Amenity[];
+}) => {
+  const amenitiesWithIcons = defaultValue?.map(({ name, selected }) => ({
+    name,
+    selected,
+    icon: amenities.find((amenity) => amenity.name === name)!.icon,
+  }));
   const [selectedAmenities, setSelectedAmenities] = useState<Amenity[]>(
-    defaultValue || conservativeAmenities
+    amenitiesWithIcons || amenities
   );
-
   const handleChange = (amenity: Amenity) => {
     setSelectedAmenities((prev) => {
       return prev.map((a) => {
@@ -28,22 +36,23 @@ const AmenitiesInput = ({ defaultValue }: { defaultValue?: Amenity[] }) => {
         value={JSON.stringify(selectedAmenities)}
       />
       <div className="grid grid-cols-2 gap-4">
-        {selectedAmenities.map((amenity) => (
-          <div key={amenity.name} className="flex items-center space-x-2">
-            <Checkbox
-              id={amenity.name}
-              checked={amenity.selected}
-              onCheckedChange={() => handleChange(amenity)}
-            />
-            <label
-              htmlFor={amenity.name}
-              className="text-sm font-medium leading-none capitalize flex gap-x-2 items-center"
-            >
-              {amenity.name}
-              <amenity.icon className="w-4 h-4" />
-            </label>
-          </div>
-        ))}
+        {selectedAmenities.map((amenity) => {
+          return (
+            <div key={amenity.name} className="flex items-center space-x-2">
+              <Checkbox
+                id={amenity.name}
+                checked={amenity.selected}
+                onCheckedChange={() => handleChange(amenity)}
+              />
+              <label
+                htmlFor={amenity.name}
+                className="text-sm font-medium leading-none capitalize flex gap-x-2 items-center"
+              >
+                {amenity.name} <amenity.icon className="w-4 h-4" />
+              </label>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
